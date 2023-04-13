c0b5ee21820b02f07180f48edeac821662e489ee
Piotr Zdolski <dante383@protonmail.com>
2023-04-13 19:29:45 +0200

    fix filenames with spaces in them

--- vlc_gif_maker.lua
+++ vlc_gif_maker.lua
@@ -13,7 +13,7 @@ suitable to your operating system:
 To open GIF Maker: View > VLC Gif Maker
 --]]----------------------------------------
 
-default_command = 'ffmpeg -ss {start_timestamp} -to {stop_timestamp} -i {input_file} -vf "fps=15,scale=498:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 {output_path}/{output_filename}.gif'
+default_command = 'ffmpeg -ss {start_timestamp} -to {stop_timestamp} -i {input_file} -vf "fps=15,scale=498:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 "{output_path}/{output_filename}.gif"'
 
 function get_timestamp()
     local microseconds = vlc.var.get(vlc.object.input(), "time")
